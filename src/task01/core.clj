04a48b8ae8c596a6798135600b647f3d027d93ec
DenisKuzin <digiholiq@gmail.com>
2013-03-20 15:59:17 +0400

    Using reduce to walk tree by node index and some beauty fixes

--- src/task01/core.clj
+++ src/task01/core.clj
@@ -3,22 +3,40 @@
   (:gen-class))
 
 
-(defn get-h3 [li]
+(defn get-node-by-path
+  "Get tree root and walk by indexes"
+  [root & more]
   (doall
-   (filter (fn [tag] (= (tag 0) :h3)) (li 2))))
+   (reduce (fn [node index] (node index)) root more)))
 
-(defn get-href [h3]
-  ((((first h3) 2) 1) :href))
+(defn filter-by-tag
+  "Filter nodes sequence which has tag"
+  [tag seq]
+  (doall
+   (filter (fn [node] (= (node 0) tag)) seq)))
 
-(defn get-links []
+(defn extract-h3
+  "Extracts h3 tags from li's"
+  [li-seq]
+  (doall
+   (map (fn [li] (filter-by-tag :h3 (li 2))) li-seq)))
+
+(defn extract-href
+  "Extracts href attribute from h3's"
+  [h3-seq]
+  (doall
+   (map (fn [h3] ((((first h3) 2) 1) :href)) h3-seq)))
+
+(defn get-links
+  "Get links from google serp"
+  []
   (let [data (parse "clojure_google.html")
-        li-seq (doall
-                (filter (fn [tag] (= (tag 0) :li)) (((((((((((data 5) 4) 3) 2) 9) 2) 4) 5) 3) 3) 2)))
-        h3-seq (doall
-                (map get-h3 li-seq))
-        links-seq (doall
-                   (map get-href h3-seq))]
-    (vec links-seq)))
+        ol-tag (get-node-by-path data 5 4 3 2 9 2 4 5 3 3 2)
+        li-seq (filter-by-tag :li ol-tag)
+        h3-seq (extract-h3 li-seq)
+        links-seq (extract-href h3-seq)
+        links-vec (vec links-seq)]
+    links-vec))
 
 (defn -main []
   (println (str "Found " (count (get-links)) " links!")))
